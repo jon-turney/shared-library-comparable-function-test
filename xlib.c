bdab446c47a7a5c6678471b6f73b30c258968925
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-05-03 12:50:11 +0100

    Obvious fixes for x86_64

--- xlib.c
+++ xlib.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
+#ifdef __x86_64__
+asm (".data\n\
+ .globl _XtInherit        \n\
+ _XtInherit:              \n\
+    jmp *_y(%rip)         \n\
+_y: .quad __XtInherit     \n\
+    .text                 \n");
+#else
 asm (".data\n\
  .globl __XtInherit        \n\
  __XtInherit:      jmp *_y \n\
  _y: .long ___XtInherit    \n\
  .text                     \n");
+#endif
 
 #define _XtInherit __XtInherit
 void _XtInherit ()
