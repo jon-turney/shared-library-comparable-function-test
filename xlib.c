bad533ca5a6d0c0194a18e4cf3c40420b3d3d074
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-05-03 12:45:46 +0100

    Reindent

--- xlib.c
+++ xlib.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
- asm (".data\n\
- .globl __XtInherit 		   \n\
+asm (".data\n\
+ .globl __XtInherit        \n\
  __XtInherit:      jmp *_y \n\
-	_y: .long ___XtInherit   \n\
- 		.text									\n"); 
+ _y: .long ___XtInherit    \n\
+ .text                     \n");
 
 #define _XtInherit __XtInherit
- void _XtInherit ()
+void _XtInherit ()
 {
   printf ("_XtInherit ()\n");
 }
-
-
--- xtest.c
+++ xtest.c
@@ -1,15 +1,14 @@
 #include "x.h"
 #include <stdio.h>
 
-
 int
 main (int argc, char **argv)
 {
   printf ("x1.callback: %p\n", x1.callback);
   printf ("x2.callback: %p\n", x2.callback);
   printf ("_XtInherit : %p\n", _XtInherit);
-	x1.callback(); 
-	x2.callback(); 
-	
+  x1.callback();
+  x2.callback();
+
   return 0;
 }
